c8e32a7c576336d86926f9dfa7c224a2e41e5627
João <joao.pereira2277@usp.br>
2025-05-17 10:29:38 -0300

    Adding new plugins, themes and more

---
+++ Frontend/plugins/lucide.ts
@@ -0,0 +1,8 @@
+import { defineNuxtPlugin } from '#app'
+import * as lucide from 'lucide-vue-next'
+
+export default defineNuxtPlugin((nuxtApp) => {
+  for (const [name, component] of Object.entries(lucide)) {
+    nuxtApp.vueApp.component(name, component)
+  }
+})
