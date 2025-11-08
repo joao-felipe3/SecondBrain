9ead171f8f2636880495ccce9668e0e8c1373f80
João Felipe <joao.pereira2277@gmail.com>
2025-11-07 19:02:30 -0500

    feat: fix refresh button icon

--- Frontend/plugins/vuetify.ts
+++ Frontend/plugins/vuetify.ts
@@ -3,11 +3,21 @@ import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import { defineNuxtPlugin } from '#app'
 import 'vuetify/styles'
+// Material Design Icons (font-based) - used by mdi-* identifiers in templates
+import '@mdi/font/css/materialdesignicons.css'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
 
 export default defineNuxtPlugin((nuxtApp) => {
   const vuetify = createVuetify({
     components,
     directives,
+    icons: {
+      defaultSet: 'mdi',
+      aliases,
+      sets: {
+        mdi,
+      },
+    },
   })
 
   nuxtApp.vueApp.use(vuetify)
